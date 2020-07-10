a14cc055366b20a4ff6dfa9ffde2c33668ec3e85
Edward Phillips <edd0ddi0@gmail.com>
2020-07-10 16:01:30 +0100

    Green: notation Converter should be able to convert as string C1 to 16

--- jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
@@ -1,7 +1,15 @@
 function notationConverter(string) {
   if (string[0] == "A") {
     return parseInt(string[1]) - 1;
-  } else {
+  } else if (string[0] == "B") {
     return 8 + parseInt(string[1]) - 1;
-  }
+  } else {
+    const rowPosition = 25 - (90-string.charCodeAt(0));
+    console.log("AZ".charCodeAt(0))
+    console.log(string.charCodeAt(0));
+    console.log(rowPosition);
+    const colPosition = parseInt(string[1]) - 1;
+    const result = rowPosition * 8 + colPosition;
+    return result;
+  };
 }
