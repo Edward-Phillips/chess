4daa57b2125bcf6b0e3972830576d0cc6515f9d7
Edward Phillips <edd0ddi0@gmail.com>
2020-07-10 15:22:39 +0100

    Make basic board, cell, player classes & notation converter first tests

---
+++ jasmine-standalone-3.5.0 (1) copy/src/Board.js
@@ -0,0 +1,26 @@
+class Board {
+  constructor() {
+    this.cells = []
+    this.currentTurn = 0
+  };
+
+  generateCells() {
+    let colour = false;
+    for (let index = 0; index < 64; index++) {
+      if (index % 2 == 0) {
+        colour = true;
+      }
+      const element = new Cell(colour);
+      this.cells.push(element);
+    }
+  };
+  turn() {
+    if (this.currentTurn % 2 == 0) {
+      return "White"
+    }
+    return "Black"
+  };
+  incTurn() {
+    this.currentTurn += 1;
+  };
+};
