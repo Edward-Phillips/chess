e0a3ca3b11add7b3642f3a91b3fa6e5771e01e10
Edward Phillips <edd0ddi0@gmail.com>
2020-07-10 16:04:14 +0100

    RF: remove console logs

--- jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
@@ -1,8 +1,5 @@
 function notationConverter(string) {
     const rowPosition = 25 - (90-string.charCodeAt(0));
-    console.log("AZ".charCodeAt(0))
-    console.log(string.charCodeAt(0));
-    console.log(rowPosition);
     const colPosition = parseInt(string[1]) - 1;
     const result = rowPosition * 8 + colPosition;
     return result;
