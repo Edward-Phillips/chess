1d81d9527ed5f6563c6e42a55e2794db34ab1fb6
Edward Phillips <edd0ddi0@gmail.com>
2020-07-10 16:28:31 +0100

    RF: simplify error handling

--- jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
@@ -1,10 +1,9 @@
 function notationConverter(string) {
-  if (string.length > 2) {
-    throw new Error('invalid input')
-  }
   const rowPosition = 25 - (90-string.charCodeAt(0));
   const colPosition = parseInt(string[1]) - 1;
-  if (rowPosition > 7 || colPosition > 7) {
+  const offTheBoard = rowPosition > 7 || colPosition > 7;
+  const tooLong = string.length > 2;
+  if (tooLong || offTheBoard) {
     throw new Error('invalid input')
   }
   const result = rowPosition * 8 + colPosition;
