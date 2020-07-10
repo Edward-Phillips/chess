ca503dd012ee6bdac1503469d8f373156763b6b2
Edward Phillips <edd0ddi0@gmail.com>
2020-07-10 16:25:48 +0100

    Green: should not accept inputs with a length greater than 2

--- jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
@@ -1,9 +1,12 @@
 function notationConverter(string) {
-    const rowPosition = 25 - (90-string.charCodeAt(0));
-    const colPosition = parseInt(string[1]) - 1;
-    if (rowPosition > 7 || colPosition > 7) {
-      throw new Error("invalid input")
-    }
-    const result = rowPosition * 8 + colPosition;
-    return result;
+  if (string.length > 2) {
+    throw new Error('invalid input')
+  }
+  const rowPosition = 25 - (90-string.charCodeAt(0));
+  const colPosition = parseInt(string[1]) - 1;
+  if (rowPosition > 7 || colPosition > 7) {
+    throw new Error('invalid input')
+  }
+  const result = rowPosition * 8 + colPosition;
+  return result;
 }
