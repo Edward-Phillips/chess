fb832e25cbd25194dd783746bf0b93627db304b9
Edward Phillips <edd0ddi0@gmail.com>
2020-07-10 16:21:07 +0100

    Green: should not accept A1-style inputs beyond H, 8

--- jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
@@ -1,6 +1,12 @@
 function notationConverter(string) {
     const rowPosition = 25 - (90-string.charCodeAt(0));
+    if (rowPosition > 7) {
+      throw new Error("invalid input")
+    }
     const colPosition = parseInt(string[1]) - 1;
+    if (colPosition > 7) {
+      throw new Error("invalid input")
+    }
     const result = rowPosition * 8 + colPosition;
     return result;
 }
