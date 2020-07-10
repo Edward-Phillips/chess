8df523e252f20b02b7acacc18dd6479d4351c5c7
Edward Phillips <edd0ddi0@gmail.com>
2020-07-10 16:22:26 +0100

    RF: simplify notationconverter error handling

--- jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
@@ -1,10 +1,7 @@
 function notationConverter(string) {
     const rowPosition = 25 - (90-string.charCodeAt(0));
-    if (rowPosition > 7) {
-      throw new Error("invalid input")
-    }
     const colPosition = parseInt(string[1]) - 1;
-    if (colPosition > 7) {
+    if (rowPosition > 7 || colPosition > 7) {
       throw new Error("invalid input")
     }
     const result = rowPosition * 8 + colPosition;
