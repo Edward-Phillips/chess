62467cece8e6bd3d0f1fcd89b86c128a2e1e6f1c
Edward Phillips <edd0ddi0@gmail.com>
2020-07-11 01:23:49 +0100

    Green: should not accept A0 as an input

--- jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/NotationConverter.js
@@ -1,9 +1,12 @@
 function notationConverter(string) {
   const rowPosition = 25 - (90-string.charCodeAt(0));
   const colPosition = parseInt(string[1]) - 1;
-  const offTheBoard = rowPosition > 7 || colPosition > 7;
+  const overTheBoard = rowPosition > 7 || colPosition > 7;
+  const underTheBoard = rowPosition < 0 || colPosition < 0;
+  const offTheBoard = overTheBoard || underTheBoard;
   const tooLong = string.length > 2;
-  if (tooLong || offTheBoard) {
+  const errorCriteria = tooLong || offTheBoard
+  if (errorCriteria) {
     throw new Error('invalid input')
   }
   const result = rowPosition * 8 + colPosition;
