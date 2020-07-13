4407562024f741a524310bde23a82d604a5fbee7
Edward Phillips <edd0ddi0@gmail.com>
2020-07-13 13:58:10 +0100

    Red: it has no contents when a piece is not given at cell creation

--- jasmine-standalone-3.5.0 (1) copy/spec/CellSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/CellSpec.js
@@ -13,19 +13,32 @@ describe('cell class', () => {
     const colour = false,
     piece = "pawn",
     square = new Cell(colour, piece);
-    expect(square.contents[0]).toEqual("pawn");
+    expect(square.contents[0]).toEqual(piece);
   });
   it('can return the piece stored in the cell', () => {
     const colour = false,
     piece = "pawn",
     square = new Cell(colour, piece);
-    expect(square.extractPiece()).toEqual("pawn");
+    expect(square.extractPiece()).toEqual(piece);
   });
   it('returning the piece removes the piece from the cell', () => {
     const colour = false,
     piece = "pawn",
     square = new Cell(colour, piece);
     square.extractPiece();
+    expect(square.contents.length).toEqual(0);
+  });
+  it('can add a piece to the cell', () => {
+    const colour = false,
+    piece = "knight",
+    square = new Cell(colour);
+    square.insertPiece(piece);
+    expect(square.extractPiece()).toEqual(piece);
+  });
+  it('has no contents when a piece is not given at cell creation', () => {
+    const colour = false,
+    piece = "knight",
+    square = new Cell(colour);
     expect(square.contents.length).toEqual(0);
   });
   
