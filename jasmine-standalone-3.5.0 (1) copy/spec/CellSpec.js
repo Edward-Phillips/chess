a2b864cbb3c948ab8b81584cbbe9b2d3b533f36c
Edward Phillips <edd0ddi0@gmail.com>
2020-07-13 12:01:13 +0100

    Red:  can store a piece in the cell

--- jasmine-standalone-3.5.0 (1) copy/spec/CellSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/CellSpec.js
@@ -1,12 +1,18 @@
 describe('cell class', () => {
-  it('can tell me if it is white or black pt1',() => {
-    colour = true
+  it('can tell me if it is white or black pt1', () => {
+    colour = true;
     square = new Cell(colour);
     expect(square.colour).toEqual(true);
   });
-  it('can tell me if it is white or black pt2',() => {
-    colour = false
+  it('can tell me i;f it is white or black pt2', () => {
+    colour = false;
     square = new Cell(colour);
     expect(square.colour).toEqual(false);
+  });
+  it('can store a piece in the cell', () => {
+    const colour = false,
+    piece = "pawn",
+    square = new Cell(colour, piece);
+    expect(square.contents).toEqual("pawn");
   });
 })
