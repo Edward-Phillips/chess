dc05f0f93cd1aac92573614facf814b8068cb1db
Edward Phillips <edd0ddi0@gmail.com>
2020-07-29 14:41:57 +0100

    R: can determine the positions it can move to when not in starting position

--- jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
@@ -5,4 +5,10 @@ describe('Pawn Class', () => {
     const piece = new Pawn(colour, startingPosition);
     expect(piece.moves()).toEqual(["A3", "A4"])
   })
+  it('can determine the positions it can move to pt 2', () => {
+    const colour = "white";
+    const startingPosition = "A3";
+    const piece = new Pawn(colour, startingPosition);
+    expect(piece.moves()).toEqual(["A4"])
+  })
 })
