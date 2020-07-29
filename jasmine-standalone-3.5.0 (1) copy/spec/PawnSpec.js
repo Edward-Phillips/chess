e217c486fc0e5dc081012f1de9866ca0263768d9
Edward Phillips <edd0ddi0@gmail.com>
2020-07-29 14:42:36 +0100

    RF: adjust test title to reflect test

--- jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
@@ -5,7 +5,7 @@ describe('Pawn Class', () => {
     const piece = new Pawn(colour, startingPosition);
     expect(piece.moves()).toEqual(["A3", "A4"])
   })
-  it('can determine the positions it can move to pt 2', () => {
+  it('can determine the positions it can move to when not in starting position', () => {
     const colour = "white";
     const startingPosition = "A3";
     const piece = new Pawn(colour, startingPosition);
