c0856dc87cefce6089099767b3dbb0a394bea43c
Edward Phillips <edd0ddi0@gmail.com>
2020-07-29 14:42:19 +0100

    G: can determine the positions it can move to when not in starting position

--- jasmine-standalone-3.5.0 (1) copy/src/Pawn.js
+++ jasmine-standalone-3.5.0 (1) copy/src/Pawn.js
@@ -4,6 +4,13 @@ class Pawn extends Piece {
   };
 
   moves () {
-    return ['A3', "A4"]
+    const numberPosition = parseInt(this.position[1]);
+    if (numberPosition == 2) {
+      const options = [numberPosition + 1, numberPosition + 2]
+      return [this.position[0]+options[0], this.position[0] + options[1]]
+    } else {
+      const options = numberPosition + 1;
+      return [this.position[0] + options ]
+    }
   }
 }
