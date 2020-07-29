3ba7f0c5c1a8e9a37c88de88d4017eeeafefa8c1
Edward Phillips <edd0ddi0@gmail.com>
2020-07-29 13:35:54 +0100

    R: piece stores tehcurrent position on the board

--- jasmine-standalone-3.5.0 (1) copy/spec/PieceSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/PieceSpec.js
@@ -1,7 +1,14 @@
 describe('Piece class', () => {
   it('has a colour', () => {
     const colour = "black";
-    const pawn = new Piece(colour);
+    const startPoint = "nil"
+    const pawn = new Piece(colour, startPoint);
     expect(pawn.colour).toEqual(colour);
   });
+  it('stores the current position on the board', () => {
+    const colour = "black";
+    const position = "A1"
+    const pawn = new Piece(colour, position)
+    expect(pawn.position).toEqual(position);
+  })
 })
