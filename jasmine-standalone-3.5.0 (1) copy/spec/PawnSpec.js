8a8278d165f9269927319cf5f6188c929deb4471
Edward Phillips <edd0ddi0@gmail.com>
2020-07-30 14:10:29 +0100

    R: movement tests for black pawns

--- jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
@@ -17,5 +17,22 @@ describe('Pawn Class', () => {
     const piece = new Pawn(colour, startingPosition);
     expect(piece.moves()).toEqual([]);
   });
-  
+  it('can determine the positions it can move to when the piece is black', () => {
+    const colour = "black";
+    const startingPosition = "A7";
+    const piece = new Pawn(colour, startingPosition);
+    expect(piece.moves()).toEqual(["A6", "A5"])
+  });
+  it('can determine the positions it can move to when not in starting position', () => {
+    const colour = "black";
+    const startingPosition = "A3";
+    const piece = new Pawn(colour, startingPosition);
+    expect(piece.moves()).toEqual(["A2"])
+  });
+  it('Does not allow moves beyond the 8th row', () => {
+    const colour = "black";
+    const startingPosition = "A1";
+    const piece = new Pawn(colour, startingPosition);
+    expect(piece.moves()).toEqual([]);
+  });
 })
