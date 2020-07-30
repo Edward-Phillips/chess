ecb25f570640aa2b6b8d9141d0701aa819ba6314
Edward Phillips <edd0ddi0@gmail.com>
2020-07-30 16:50:10 +0100

    Refactor: Store cells in Object instead of Array

--- jasmine-standalone-3.5.0 (1) copy/src/Board.js
+++ jasmine-standalone-3.5.0 (1) copy/src/Board.js
@@ -1,18 +1,22 @@
 class Board {
 
   constructor() {
-    this.cells = []
+    this.cells = {}
     this.currentTurn = 0
   };
 
   generateCells() {
     let colour = false;
+    const charCode = "A".charCodeAt(0);
     for (let index = 0; index < 64; index++) {
       if (index % 2 == 0) {
         colour = true;
       }
       const element = new Cell(colour);
-      this.cells.push(element);
+      const currentLetter = String.fromCharCode(charCode + index % 8)
+      const currentNumber = parseInt(9 - (index + 1) / 8);
+      const cellAddress = currentLetter + currentNumber;
+      this.cells[cellAddress] = index;
     }
   };
 
