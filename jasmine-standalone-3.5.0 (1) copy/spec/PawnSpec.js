6fbf28caf453ef5199848810f037531a753b46e6
Edward Phillips <edd0ddi0@gmail.com>
2020-07-30 13:53:47 +0100

    R: Does not allow moves beyond the 8th row

--- jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/PawnSpec.js
@@ -4,11 +4,18 @@ describe('Pawn Class', () => {
     const startingPosition = "A2";
     const piece = new Pawn(colour, startingPosition);
     expect(piece.moves()).toEqual(["A3", "A4"])
-  })
+  });
   it('can determine the positions it can move to when not in starting position', () => {
     const colour = "white";
     const startingPosition = "A3";
     const piece = new Pawn(colour, startingPosition);
     expect(piece.moves()).toEqual(["A4"])
-  })
+  });
+  it('Does not allow moves beyond the 8th row', () => {
+    const colour = "white";
+    const startingPosition = "A8";
+    const piece = new Pawn(colour, startingPosition);
+    expect(piece.moves()).toEqual([]);
+  });
+  
 })
