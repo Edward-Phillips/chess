dc6ee02aac5534215b6bffd618a43295a760322e
Edward Phillips <edd0ddi0@gmail.com>
2020-07-30 13:53:58 +0100

    G: does not allow moves beyond the 8th row

--- jasmine-standalone-3.5.0 (1) copy/src/Pawn.js
+++ jasmine-standalone-3.5.0 (1) copy/src/Pawn.js
@@ -8,6 +8,8 @@ class Pawn extends Piece {
     if (numberPosition == 2) {
       const options = [numberPosition + 1, numberPosition + 2]
       return [this.position[0]+options[0], this.position[0] + options[1]]
+    } else if (numberPosition >= 8) {
+      return []
     } else {
       const options = numberPosition + 1;
       return [this.position[0] + options ]
