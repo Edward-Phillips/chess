d8c2bbfec3920aebc8bd5cdb5140c7ece47077ee
Edward Phillips <edd0ddi0@gmail.com>
2020-08-07 21:19:17 +0100

    Red: it can determine where it can move to from H1

---
+++ jasmine-standalone-3.5.0 (1) copy/spec/RookSpec.js
@@ -0,0 +1,8 @@
+describe("Rook Class", () => {
+  it('can determine where it can move to from H1', () => {
+    const colour = "white";
+    const startingPosition = "H1";
+    const piece = new Rook(colour, startingPosition);
+    expect(piece.moves().sort()).toEqual(["A1","B1","C1","D1","E1","F1","G1","H2","H3","H4","H5","H6","H7","H8"].sort())
+  })
+})
