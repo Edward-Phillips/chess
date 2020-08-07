7ab03e39baaa9163ec5122abf251bb6d6e37b243
Edward Phillips <edd0ddi0@gmail.com>
2020-08-07 21:38:27 +0100

    RF: Add CharCode limit values to class from child

--- jasmine-standalone-3.5.0 (1) copy/src/Piece.js
+++ jasmine-standalone-3.5.0 (1) copy/src/Piece.js
@@ -2,5 +2,7 @@ class Piece {
   constructor (colour, startingPosition) {
     this.colour = colour;
     this.position = startingPosition;
+    this.charCodeMin = 65;
+    this.charCodeMax = 72;
   }
 }
