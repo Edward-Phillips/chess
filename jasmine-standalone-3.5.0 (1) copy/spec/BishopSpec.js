58f1ee8add817aa39030bcfe189a948334c3c40e
Edward Phillips <edd0ddi0@gmail.com>
2020-08-07 19:50:44 +0100

    G: can determine where it can move to from E4

--- jasmine-standalone-3.5.0 (1) copy/spec/BishopSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/BishopSpec.js
@@ -5,4 +5,10 @@ describe("Bishop Class", () => {
     const piece = new Bishop(colour, startingPosition);
     expect(piece.moves().sort()).toEqual(["A3","B2","D2","E3","F4","G5","H6"].sort());
   });
+  it('can determine where it can move to from E4', () => {
+    const colour = "white";
+    const startingPosition = "E4";
+    const piece = new Bishop(colour, startingPosition);
+    expect(piece.moves().sort()).toEqual(["B1","C2","D3","F5","G6","H7","H1","G2","F3","D5","C6","B7","A8"].sort());
+  });
 })
