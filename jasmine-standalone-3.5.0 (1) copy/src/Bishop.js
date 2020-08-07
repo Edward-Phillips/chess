f063139531368ac8461c747586669cb0fbb4d6fe
Edward Phillips <edd0ddi0@gmail.com>
2020-08-07 19:32:42 +0100

    RF: movement options returned based on input instead of hardcoded values

--- jasmine-standalone-3.5.0 (1) copy/src/Bishop.js
+++ jasmine-standalone-3.5.0 (1) copy/src/Bishop.js
@@ -1,8 +1,40 @@
 class Bishop extends Piece {
   constructor(colour, piece) {
     super(colour, piece);
+    this.charCodeMin = 65;
+    this.charCodeMax = 72;
   };
   moves () {
-    return ["A3","B2","D2","E3","F4","G5","H6"]
+    const numberPosition = parseInt(this.position[1]);
+    const charValue = this.position[0].charCodeAt(0);
+    let increment = 0;
+    const results = [];
+    for (let index = charValue + 1; index <= this.charCodeMax; index++) {
+      increment += 1;
+      const character = String.fromCharCode(index);
+      const numberPositive = numberPosition + increment;
+      const numberNegative = numberPosition - increment;
+      if (numberPositive <= 8) {
+        results.push(character + numberPositive);
+      }
+      if (numberNegative >= 1) {
+        results.push(character + numberNegative);
+      }
+    }
+    increment = 0;
+    for (let index = charValue -1; index >= this.charCodeMin; index--) {
+      increment += 1;
+      const character = String.fromCharCode(index)
+      const numberPositive = numberPosition + increment;
+      const numberNegative = numberPosition - increment;
+      if (numberPositive <= 8) {
+        results.push(character + numberPositive);
+        console.log(character + numberPositive);
+      }
+      if (numberNegative >= 1) {
+        results.push(character + numberNegative);
+      }
+    }
+    return results
   }
 }
