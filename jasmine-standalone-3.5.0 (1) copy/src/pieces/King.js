e4e1257fe5b896b252eb234f2b45a1915d4ad694
Edward Phillips <edd0ddi0@gmail.com>
2020-08-08 10:53:12 +0100

    G: can determine where it can move to from E4

--- jasmine-standalone-3.5.0 (1) copy/src/pieces/King.js
+++ jasmine-standalone-3.5.0 (1) copy/src/pieces/King.js
@@ -3,6 +3,23 @@ class King extends Piece {
     super(colour, position);
   }
   moves() {
-    return ["D3","E2","F3","F4","F5","E5","D5","D4"]
+    const charValue = this.position[0].charCodeAt(0);
+    const numberPosition = parseInt(this.position[1]);
+    const startingPosition = numberPosition - 1
+    const results = [];
+    for (let index = charValue - 1; index <= charValue + 1; index++) {
+      if (index >= this.charCodeMin && index <= this.charCodeMax) {
+        const character = String.fromCharCode(index);
+        if (startingPosition >= 1) {
+          results.push(character + startingPosition);
+        }
+        results.push(character + (startingPosition + 1))
+        if ( startingPosition + 2 <= 8) {
+          results.push(character + (startingPosition + 2));
+        }
+      }
+    
+    }
+    return results.filter(x=> x !==this.position);
   }
 }
