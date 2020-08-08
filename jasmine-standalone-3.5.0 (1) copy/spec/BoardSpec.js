de2cc39cc27a38e33e5c311a4a62bd1795385ec3
Edward Phillips <edd0ddi0@gmail.com>
2020-08-08 12:19:24 +0100

    R: setting the pawns on the board puts a black pawn in A7

--- jasmine-standalone-3.5.0 (1) copy/spec/BoardSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/BoardSpec.js
@@ -5,13 +5,18 @@ describe('board class', () => {
   });
   it(' can store the cells of the board', () => {
     const chessboard = new Board;
-    expect(chessboard.cells.length).toEqual(0);
+    expect(typeof(chessboard.cells)).toEqual(typeof([]));
   });
   it(' can populate the board with 64 cells', () => {
     const chessboard = new Board;
     chessboard.generateCells();
-    expect(chessboard.cells.length).toEqual(64);
+    expect(Object.keys(chessboard.cells).length).toEqual(64);
   });
+  it(' can return the cell given the A1 input', () => {
+    const chessboard = new Board();
+    chessboard.generateCells();
+    expect(chessboard.cells["A1"]).toBeInstanceOf(Cell);
+  })
   it(" can determine who's turn it is pt1", ()=> {
     const chessboard = new Board;
     chessboard.generateCells();
@@ -22,5 +27,19 @@ describe('board class', () => {
     chessboard.generateCells();
     chessboard.incTurn();
     expect(chessboard.turn()).toEqual('Black')
+  });
+  it(" setting the pawns on the board puts a black pawn in A7 part 1", () => {
+    const chessboard = new Board;
+    const colour = "black"
+    chessboard.generateCells();
+    chessboard.setPawns();
+    expect(chessboard.cells["A7"].extractPiece().colour).toEqual("black")
+  });
+  it(" setting the pawns on the board puts a black pawn in A7 part 2", () => {
+    const chessboard = new Board;
+    const colour = "black"
+    chessboard.generateCells();
+    chessboard.setPawns();
+    expect(chessboard.cells["A7"].extractPiece()).toBeInstanceOf(Pawn);
   });
 })
