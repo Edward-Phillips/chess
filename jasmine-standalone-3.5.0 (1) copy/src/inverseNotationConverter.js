cfd1fad5a35ea198673d975a1495a99dfdb7d7dc
Edward Phillips <edd0ddi0@gmail.com>
2020-08-09 14:48:20 +0100

    G: can take an input 0,1 and return A2

--- jasmine-standalone-3.5.0 (1) copy/src/inverseNotationConverter.js
+++ jasmine-standalone-3.5.0 (1) copy/src/inverseNotationConverter.js
@@ -1,3 +1,7 @@
-function inverseNotationConverter () {
-  return "A1"
+function inverseNotationConverter (inputArray) {
+  if(inputArray[0] == 0 && inputArray[1] == 0) {
+    return "A1"
+  } else {
+    return "A2"
+  }
 }
