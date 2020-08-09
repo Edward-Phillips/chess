11f1a15f024ca147b38ed2519dc16d541b61de98
Edward Phillips <edd0ddi0@gmail.com>
2020-08-09 14:48:09 +0100

    R:  can take an input 0,1 and return A2

--- jasmine-standalone-3.5.0 (1) copy/spec/inverseNotationConverterSpec.js
+++ jasmine-standalone-3.5.0 (1) copy/spec/inverseNotationConverterSpec.js
@@ -1,5 +1,8 @@
-describe('inverse Converter function', () => {
+describe('inverse notation converter function', () => {
   it('can take an input [0,0] and return A1', () => {
     expect(inverseNotationConverter([0,0])).toEqual("A1");
+  })
+  it('can take an input [0,1] and return A2', () => {
+    expect(inverseNotationConverter([0,1])).toEqual("A2");
   })
 })
